ba24d29e5e46204b5704888af06093aa63336398
Gunnar Siréus <gunnar.sireus@gmail.com>
2024-08-03 13:50:08 +0200

    Added JWT Authorization header using the Bearer scheme to Swagger

--- ReactHomepage/Program.cs
+++ ReactHomepage/Program.cs
@@ -117,6 +117,31 @@ builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "SireusCore", Version = "v1" });
     c.DocumentFilter<CustomDocumentFilter>();  // Register the custom document filter
+
+    // Add the JWT bearer token authentication
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                },
+                new string[] { }
+            }
+        });
 });
 
 var app = builder.Build();
